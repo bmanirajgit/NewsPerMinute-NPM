d67aff344eb671ae86ad7728b0de29d575b59f9a
bmanirajgit <bmaniraj@gmu.edu>
2021-10-31 00:52:58 -0400

    Gen upload

---
+++ src/main/java/host/heroku/bot/Main.java
@@ -0,0 +1,22 @@
+package host.heroku.bot;
+
+import host.heroku.bot.commands.Ping;
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+
+public class Main {
+
+    public static String Prefix = "tt ";
+    public static void main(String[] args){
+
+        DiscordApi Bot = new DiscordApiBuilder()
+                .setToken(System.getenv().get("TOKEN"))
+                .login().join();
+
+        Bot.addListener(new Ping());
+
+        System.out.println("We are hosted");
+    }
+
+
+}
---
+++ src/main/java/host/heroku/bot/commands/Ping.java
@@ -0,0 +1,17 @@
+package host.heroku.bot.commands;
+
+import host.heroku.bot.Main;
+import org.javacord.api.event.message.MessageCreateEvent;
+import org.javacord.api.listener.message.MessageCreateListener;
+
+public class Ping implements MessageCreateListener {
+
+    @Override
+    public void onMessageCreate(MessageCreateEvent message) {
+
+        if (message.getMessageContent().equalsIgnoreCase(Main.Prefix + "Ping")){
+
+            message.getChannel().sendMessage("Pong");
+        }
+    }
+}
