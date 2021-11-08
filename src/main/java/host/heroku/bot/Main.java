6a1ac40fadc94cd80ed6ec4f72346ec622bbfb8d
bmanirajgit <bmaniraj@gmu.edu>
2021-11-07 19:33:35 -0500

    Changed command prefix

--- src/main/java/host/heroku/bot/Main.java
+++ src/main/java/host/heroku/bot/Main.java
@@ -7,7 +7,7 @@ import org.javacord.api.DiscordApiBuilder;
 
 public class Main {
 
-    public static String Prefix = "tt ";
+    public static String Prefix = "npm ";
     public static void main(String[] args){
 
         DiscordApi Bot = new DiscordApiBuilder()
