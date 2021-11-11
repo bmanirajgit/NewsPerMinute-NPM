f4c3c257c3a5152ce90119fbe438f0320db6b3d4
gjacobr <90492384+gjacobr@users.noreply.github.com>
2021-11-11 13:03:29 -0500

    Add WPM and Accuracy algorithms
    Added the algorithms for calculating WPM and accuracy as static methods so they can be used independently in main. Accuracy algorithm is not as robust as it should be

--- src/main/java/host/heroku/bot/commands/CalculateStats.java
+++ src/main/java/host/heroku/bot/commands/CalculateStats.java
@@ -1,29 +1,34 @@
-package host.heroku.bot.commands;
+package com.github.gjacobr;
 import java.util.*;
-import java.util.*;
-import org.javacord.api.DiscordApi;
-import org.javacord.api.DiscordApiBuilder;
-import org.javacord.api.entity.DiscordEntity;
-import org.javacord.api.entity.Icon;
-import org.javacord.api.entity.message.Message;
-import org.javacord.api.entity.message.MessageAuthor;
-import org.javacord.api.entity.message.MessageBuilder;
-import org.javacord.api.entity.message.embed.EmbedBuilder;
-import org.javacord.api.entity.user.User;
 
-import java.awt.*;
 public class CalculateStats {
     private double wpm;
     private double accuracy;
     private String errors;
 
-    public double calculateWPM(int wordCount, double time){
-        return 0.0;
+    public static double calculateWPM(int wordCount, double time){
+        /* definitely works */
+        return  wordCount/(time/60);
+
     }
 
-    public double calculateAccuracy(String input, String excerpt){
+    public static double calculateAccuracy(String input, String excerpt){
+        /* If you add an extra character in the middle of it, it will count the rest of the input as incorrect */
+        char[] input3 = input.toCharArray();
+        int usersize = input3.length;
+        char[] input4 = excerpt.toCharArray();
+        int origsize = input4.length;
+        int index;
+        double countCorrect = 0;
+        for (index = 0; index < usersize; index += 1) {
+            //System.out.println("input3index " + input3[index] + " input4index " + input4[index] + "\n");
 
-        return 99.34;
+            if (input3[index] == input4[index]) {
+                //System.out.println("W input3index " + input3[index] + " input4index " + input4[index] + "\n");
+                countCorrect += 1.0;
+            }
+        }
+        return (countCorrect / (double) origsize) * 100;
     }
 
     public void showErrors(String excerpt, String input){
