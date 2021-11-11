2243361cfccb4fa1a99642d4a2c032567f0cb40e
Ah179 <85572438+Ah179@users.noreply.github.com>
2021-11-11 10:38:05 -0500

    Update ArticleScrape.java

--- src/main/java/host/heroku/bot/commands/ArticleScrape.java
+++ src/main/java/host/heroku/bot/commands/ArticleScrape.java
@@ -18,6 +18,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.Random;
 
 import java.awt.*;
 public class ArticleScrape {
@@ -36,18 +37,41 @@ public class ArticleScrape {
         /*should add another function for scrapper*/
         return "getText";
     }
+    /* For each article or Scrapper should obtain the following:
+     * 1. Title
+     * 2. Summary
+     * 3. Link to article
+     **/
     public void readyArticle() {
         String url1 = "https://apnews.com/";
         String url2 = "https://www.nbcnews.com/?icid=nav_bar_logo";
+        String url3 = "https://www.wsj.com/";
+        String url4 = "https://www.reuters.com/";
+        String url5 = "https://www.bbc.com/";
+        int rand = new Random();
+        int random = rand.nextInt((1-5)+1)+1);
         System.out.println("This is hell");
         try {
-        Document document = Jsoup.connect(url1).get();
-        Elements allLinks = document.select("a[href]");
-          
+        /*This chooses the article based on the random number generated*/
+        Switch (random){
+            case 1: setArticle(url1);
+                    break;
+            case 2: setArticle(url2);
+                    break;
+            case 3: setArticle(url3);
+                    break;
+            case 4: setArticle(url4);
+                    break;
+            case 5: setArticle(url5);
+                    break;
+        }
+        Document document = Jsoup.connect(getArticle()).get();
+        Elements allLinks = document.select("a[href]"); //what does this part do??? DOES THIS GET ALL THE LINKS IN THE LANDING PAGE 
+            //IF SO WE COULD LOOK AT THE TIME AND CHOOSE THE MOSt recent one :) THEN we can get set the link, find the summary 
+            // set the summary and have our final thingy to present 
+            
         for(Element link: allLinks) {
-                
-                String absoluteUrl = link.attr("abs:href");
-                
+                String absoluteUrl = link.attr("abs:href"); //DOES THIS GET ALL THE LINKS ON THE LANDING PAGE 
                 System.out.println("Absolute URL: " + absoluteUrl);
         }
             
@@ -57,7 +81,6 @@ public class ArticleScrape {
         
     }
     public String getArticle(){
-        
         return this.article;
     }
 
