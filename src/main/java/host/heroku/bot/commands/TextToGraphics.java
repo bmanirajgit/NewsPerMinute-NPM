837265af2fe87f447176e527e113b7421e335be3
bmanirajgit <bmaniraj@gmu.edu>
2021-11-12 01:35:32 -0500

    test

--- src/main/java/host/heroku/bot/commands/TextToGraphics.java
+++ src/main/java/host/heroku/bot/commands/TextToGraphics.java
@@ -18,50 +18,14 @@ import javax.imageio.ImageIO;
 public class TextToGraphics implements MessageCreateListener {
 
     public void onMessageCreate(MessageCreateEvent txtGraph) {
-        String text = "Hello";
-
-        /*
-           Because font metrics is based on a graphics context, we need to create
-           a small, temporary image so we can ascertain the width and height
-           of the final image
-         */
-        BufferedImage img = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
-        Graphics2D g2d = img.createGraphics();
-        Font font = new Font("Arial", Font.PLAIN, 48);
-        g2d.setFont(font);
-        FontMetrics fm = g2d.getFontMetrics();
-        int width = fm.stringWidth(text);
-        int height = fm.getHeight();
-        g2d.dispose();
-
-        img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-        g2d = img.createGraphics();
-        g2d.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2d.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
-        g2d.setRenderingHint(RenderingHints.KEY_DITHERING, RenderingHints.VALUE_DITHER_ENABLE);
-        g2d.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
-        g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-        g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-        g2d.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
-        g2d.setFont(font);
-        fm = g2d.getFontMetrics();
-        g2d.setColor(Color.BLACK);
-        g2d.drawString(text, 0, fm.getAscent());
-        g2d.dispose();
-        try {
-            ImageIO.write(img, "png", new File("Text.png"));
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
 
         if(txtGraph.getMessageContent().equalsIgnoreCase("textG")) {
             new MessageBuilder()
                     .setEmbed(new EmbedBuilder()
-                            .setDescription("This is bot meant for both fun & learning, " +
+                            .setImage("Text.png" +
                                     "as it will help with both your typing skills and allow you to keep up with current events. " +
                                     "If you wish to know about all of the commands please use the 'npm help' command.")
-                            .setColor(Color.BLUE))
+                            .setColor(Color.GREEN))
                     .send(txtGraph.getChannel());
         }
     }
