51e43f10090782811df8edb371d714fe7d555ea1
Ah179 <85572438+Ah179@users.noreply.github.com>
2021-11-11 19:57:51 -0500

    Update ArticleScrape.java

--- src/main/java/host/heroku/bot/commands/ArticleScrape.java
+++ src/main/java/host/heroku/bot/commands/ArticleScrape.java
@@ -22,96 +22,142 @@ import java.util.Random;
 
 import java.awt.*;
 public class ArticleScrape {
-    private String article;
-    private String link;
-    private String articleText;
-    public ArticleScrape() {
-        this.article = "placeholder";
-        this.link = "placeholder";
-        this.articleText = "placeholder";
-        System.out.println("This is hell");
-    }
-    public String getText(String link){
-        /* This is just here to obtain all of the text from the article
-         * using whatever scraping library, doesn't get the excerpt yet */
-        /*should add another function for scrapper*/
-        return "getText";
-    }
-    /* For each article or Scrapper should obtain the following:
-     * 1. Title
-     * 2. Summary
-     * 3. Link to article
-     **/
-    public void readyArticle() {
-        String url1 = "https://apnews.com/";
-        String url2 = "https://www.nbcnews.com/?icid=nav_bar_logo";
-        String url3 = "https://www.wsj.com/";
-        String url4 = "https://www.reuters.com/";
-        String url5 = "https://www.bbc.com/";
-        Random rand = new Random();
-        int max = 5;
-        int min = 4;
-        int random = rand.nextInt((max-min)+1)+ min;
-        System.out.println("This is hell");
-        try {
-        /*This chooses the article based on the random number generated*/
-            /*We are sticking with the first article now just so we can work on extracting a passage*/
-        switch (1){
-            case 1: setArticle(url1);
-                    break;
-            case 2: setArticle(url2);
-                    break;
-            case 3: setArticle(url3);
-                    break;
-            case 4: setArticle(url4);
-                    break;
-            case 5: setArticle(url5);
-                    break;
-        }
-        Document document = Jsoup.connect(getArticle()).get();
-		Elements allLinks = null;
-            
-		switch (1){
-			//where does apnews keep their articles
-			case 1:
-				allLinks = document.select("a[href*=article]");
-				break;
-		    //where does nbc news keep their articles
-		    case 2:
-				break;
-			//where does wsj news lkeep their articles
-			case 3:
-				break;
-			case 4:
-				break;
-			case 5:
-				break;
+	private String article;
+	private String link;
+	private String articleText;
+	private Document document;
+	public ArticleScrape() {
+		this.article = "placeholder";
+		this.link = "placeholder";
+		this.articleText = "placeholder";
+	}
+	
+	/*This is method fetches the summary/ a paragraph of the article*/
+	public String getText( ){
+		/* This is just here to obtain all of the text from the article
+		 * using whatever scraping library, doesn't get the excerpt yet */
+		/*should add another function for scrapper*/
+		String text = "";
 
+		text = getDocument().select(".Article").select("p").text();
+		String[] summary = text.split("\\s+");
+		String[] temp = new String[summary.length];
+		StringBuffer sum = new StringBuffer();
+		int j = 0;
+		while ( j < 60){
+			temp[j] = summary[j];
+			j++;
 		}
-            
-        for(Element link: allLinks) {
-                String absoluteUrl = link.attr("abs:href"); //DOES THIS GET ALL THE LINKS ON THE LANDING PAGE 
-                System.out.println("Absolute URL: " + absoluteUrl);
-        }
-            
-         } catch (IOException e) {
-            e.printStackTrace();
-        }
-        
-    }
-    public String getArticle(){
-        return this.article;
-    }
+		for(int i = 0; i < 60; i++) {
+			sum.append(temp[i]+" ");
+		}
+		text = sum.toString();
+		return text;
+	}
+	/* For each article or Scrapper should obtain the following:
+	 * 1. Title
+	 * 2. Summary
+	 * 3. Link to article
+	 **/
+	public void readyArticle() {
+		String title = "";
+		String summary = "";
+		String url1 = "https://apnews.com/hub/trending-news?utm_source=apnewsnav&utm_medium=featured"; //a href
+		String url2 = "https://www.nbcnews.com/us-news";
+		String url3 = "https://www.wsj.com/news/us?mod=nav_left_section";
+		String url4 = "https://www.reuters.com/breakingviews";
+		String url5 = "https://www.bbc.com/news";
+		Random rand = new Random();
+		int max = 5;
+		int min = 1;
+		int random = rand.nextInt((max-min)+1)+ min;
+		//System.out.println(random);
+		try {
+			/*This chooses the article based on the random number generated*/
+			switch (1){
+				case 1: setArticle(url1);
+					break;
+				case 2: setArticle(url2);
+					break;
+				case 3: setArticle(url3);
+					break;
+				case 4: setArticle(url4);
+					break;
+				case 5: setArticle(url5);
+					break;
+			}
+			setDocument(getArticle());
+			Elements allLinks = null;
+			switch (1){
+				//where does apnews keep their articles
+				case 1:
+					allLinks = getDocument().select("a[href*=article]");
+					break;
+					//where does nbc news keep their articles
+				case 2:
 
-    public void setArticle(String article){
-        this.article = article;
-    }
+					break;
+					//where does wsj news lkeep their articles
+				case 3:
+					break;
+				case 4:
+					break;
+				case 5:
+					break;
 
-    public String getLink(){
-        return this.link;
-    }
+			}
+			 //what does this part do??? DOES THIS GET ALL THE LINKS IN THE LANDING PAGE
+			//IF SO WE COULD LOOK AT THE TIME AND CHOOSE THE MOSt recent one :) THEN we can get set the link, find the summary
+			// set the summary and have our final thingy to present
+			for(Element link: allLinks) {
+				String absoluteUrl = link.attr("abs:href"); //DOES THIS GET ALL THE LINKS ON THE LANDING PAGE
+				//Step 1 set link of article
+				setLink(absoluteUrl);
+				setArticle(absoluteUrl);
+				//System.out.println("Absolute URL: " + absoluteUrl);//print out all links obtained
+				break;
+			}
+			//Step 2 get title & Summary of article
+			setDocument(getArticle());
+			title = getDocument().title();
+			summary = getText();
+			System.out.println(title+"\n"+summary+"\n"+getLink());
 
-    public void setLink(String link){
-        this.link = link;
-    }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	/*Gets article*/
+	public String getArticle(){
+		return this.article;
+	}
+
+	/*Sets article*/
+	public void setArticle(String article){
+		this.article = article;
+	}
+	/*Sets document*/
+	public void setDocument(String article){
+		try {
+			this.document = Jsoup.connect(article).get();
+		} catch (IOException e){
+			e.printStackTrace();
+		}
+	}
+	/*return document*/
+	public Document getDocument() {
+		return this.document;
+	}
+
+	/*gets link of article*/
+	public String getLink(){
+		return this.link;
+	}
+
+	/*sets link of article*/
+	public void setLink(String link){
+		this.link = link;
+	}
 }
