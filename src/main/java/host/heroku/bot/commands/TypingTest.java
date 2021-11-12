42053c659107a3e26fa4e6d20debf4886dc6eb73
gjacobr <90492384+gjacobr@users.noreply.github.com>
2021-11-12 11:56:49 -0500

    Add basic TypingTest
    Add TypingTest using static paragraph

--- src/main/java/host/heroku/bot/commands/TypingTest.java
+++ src/main/java/host/heroku/bot/commands/TypingTest.java
@@ -1,19 +1,75 @@
 package host.heroku.bot.commands;
 
 import java.util.*;
+import host.heroku.bot.commands.;
+import host.heroku.bot.Main;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 import org.javacord.api.entity.DiscordEntity;
 import org.javacord.api.entity.Icon;
+import org.javacord.api.entity.channel.TextChannel;
+import org.javacord.api.entity.emoji.Emoji;
 import org.javacord.api.entity.message.Message;
 import org.javacord.api.entity.message.MessageAuthor;
 import org.javacord.api.entity.message.MessageBuilder;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
+import org.javacord.api.entity.server.Server;
 import org.javacord.api.entity.user.User;
+import org.javacord.api.event.message.MessageCreateEvent;
+import org.javacord.api.listener.message.MessageCreateListener;
 
 import java.awt.*;
+import java.util.concurrent.CompletableFuture;
 
-public class TypingTest {
+
+public class TypingTest implements MessageCreateListener {
+    String statictest = "The only people for me are the mad ones, the ones who are mad to live, " +
+            "mad to talk, mad to be saved, desirous of everything at the same time, " +
+            "the ones who never yawn or say a commonplace thing, but burn, burn, burn " +
+            "like fabulous yellow Roman candles exploding like spiders across the stars, " +
+            "and in the middle, you see the blue center-light pop, and everybody goes ahh...";
+    int staticLength = statictest.length();
+    private DiscordApi api;
+    public TypingTest(DiscordApi api){
+        this.api = api;
+    }
+    @Override
+    public void onMessageCreate(MessageCreateEvent test) {
+        if (test.getMessageContent().equalsIgnoreCase(Main.Prefix + "test")){
+            new MessageBuilder()
+                    .append("Go! Retype this paragraph: \n")
+                    .setEmbed(new EmbedBuilder()
+                            .setDescription(statictest)
+                            .setColor(Color.BLUE))
+                    .send(test.getChannel());
+            double start = System.currentTimeMillis();
+            MessageAuthor ma = test.getMessageAuthor();
+            System.out.println(ma);
+            api.addMessageCreateListener(input -> {
+                if (test.getMessageAuthor().equals(ma)) {
+                    String input2 = test.getMessageContent();
+                    double stop = System.currentTimeMillis();
+                    double time = (stop - start) / 1000;
+                    double accuracy = CalculateStats.calculateAccuracy(input2, statictest);
+                    double wpm = CalculateStats.calculateWPM(staticLength, time);
+                    String errors = CalculateStats.showErrors(statictest, input2);
+                    new MessageBuilder()
+                            .append("You sent: \n")
+                            .append(input2 + "\n")
+                            .setEmbed(new EmbedBuilder()
+                                    .setDescription("The time you took was: " + time + " seconds\n" +
+                                            "Your raw WPM score is: " + wpm + " words per minute.\n" +
+                                            "Your paragraph was " + accuracy + "% accurate")
+                                    .setColor(Color.green))
+
+                            .send(test.getChannel());
+                    new MessageBuilder()
+                            .append("Errors: \n" + errors)
+                            .send(test.getChannel());
+                }
+            });
+        }
+    }
     private String input = "";
     private double timeElapsed = 0.0;
     private boolean isMuted = false;
