199c22336b3d2b6e0ab56f084c676446accfc0e7
bmanirajgit <bmaniraj@gmu.edu>
2021-11-14 02:44:07 -0500

    Update botTest.java

--- src/main/java/host/heroku/bot/commands/botTest.java
+++ src/main/java/host/heroku/bot/commands/botTest.java
@@ -1,27 +1,59 @@
 package host.heroku.bot.commands;
 
 import host.heroku.bot.Main;
+import org.javacord.api.entity.message.Message;
 import org.javacord.api.entity.message.MessageBuilder;
+import org.javacord.api.entity.message.Reaction;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
+import org.javacord.api.entity.message.Message;
+import java.awt.Color;
+import java.util.*;
 
-import java.awt.*;
+
 public class botTest implements MessageCreateListener {
 
 
     @Override
     public void onMessageCreate(MessageCreateEvent check) {
+        long id;
+        long id2;
         if (check.getMessageContent().equalsIgnoreCase(Main.Prefix + "check")) {
 
             new MessageBuilder()
                     .setEmbed(new EmbedBuilder()
-                            .setTitle("NPM commands list")
-                            .setDescription("\n 'npm help' to open the help page" + "\n 'npm info' for information about the system "
-                                    + "\n 'npm run' to run the typing game")
+                            .setTitle("NPM React Test")
+                            .setDescription("React with three :thumbsup: to begin")
                             .setColor(Color.BLUE))
                     .send(check.getChannel());
+            id = check.getMessageId();
+
+            new MessageBuilder()
+                    .setEmbed(new EmbedBuilder()
+                            .setTitle("NPM React Test")
+                            .setDescription("msg2")
+                            .setColor(Color.BLUE))
+                    .send(check.getChannel());
+            id2 = check.getMessageId();
+
+            new MessageBuilder()
+                    .setEmbed(new EmbedBuilder()
+                            .setTitle("NPM React Test")
+                            .setDescription("id1 is " +  Long.toString(id) + " " + Long.toString(id2))
+                            .setColor(Color.BLUE))
+                    .send(check.getChannel());
+
+
+
+
         }
+
+
+        List<Reaction> List = new ArrayList<Reaction>();
+        //List<Reaction> people = getReactions();
+
     }
+
 
 }
