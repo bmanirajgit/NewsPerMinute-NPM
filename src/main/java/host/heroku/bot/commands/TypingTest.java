2020c6d119d94bfd620a6707a48e6a6755704a7e
Ah179 <85572438+Ah179@users.noreply.github.com>
2021-11-15 11:51:59 -0500

    Update TypingTest.java

--- src/main/java/host/heroku/bot/commands/TypingTest.java
+++ src/main/java/host/heroku/bot/commands/TypingTest.java
@@ -38,6 +38,7 @@ public class TypingTest implements MessageCreateListener {
     }
     @Override
     public void onMessageCreate(MessageCreateEvent test) {
+        boolean done = false; //plz work
         if (test.getMessageContent().equalsIgnoreCase(Main.Prefix + "test")){
             ArticleScrape scraper = new ArticleScrape();
             String summary = scraper.getText();
@@ -54,7 +55,7 @@ public class TypingTest implements MessageCreateListener {
             System.out.println(ma);
             DiscordApi testApi = this.getTTApi();
             testApi.addMessageCreateListener(input -> {
-                if (input.getMessageAuthor().equals(ma)) {
+                if (input.getMessageAuthor().equals(ma) && !(done)) {
                     String input2 = input.getMessageContent();
                     double stop = System.currentTimeMillis();
                     double time = (stop - start) / 1000;
@@ -75,7 +76,8 @@ public class TypingTest implements MessageCreateListener {
                             .append("Errors: \n" + errors)
                             .send(input.getChannel());
            
-                }
+                } 
+                done = true; //will this fixe it
             });
         }
     }
